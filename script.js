26248ebe883976b54da3305f3de19f669886abdb
dannyszyd <dannyszyd@gmail.com>
2022-06-15 16:52:42 -0400

    solve code issue (prompt call 6x instead of 5x)

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function computerPlay() {
 // [DONE] b) returns string that declares winner of round like: "You Lose! Paper > Rock"
 
 function singleRound(playerSelection , computerSelection) {
+  
   if (playerSelection === computerSelection){
     return "Tie Round!";
   } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
@@ -31,9 +32,13 @@ function singleRound(playerSelection , computerSelection) {
   }
 }
 
-let prePlayerSelection = prompt("Choose: Rock, Paper, Scissors?");
-let playerSelection = prePlayerSelection.toLowerCase(); //toString() converts the array^ from object to a string only then can it be used with .toLowerCase() , also switched from const to let because player choice is never "constant"
-const computerSelection = computerPlay();
+//const playerSelection = prompt("R, P OR S?");
+//const computerSelection = computerPlay();
+
+
+//[OLD REPLACED W/ NEW VERSION CHECK GAME()]let prePlayerSelection = prompt("Choose: Rock, Paper, Scissors?");
+//[OLD REPLACED W/ NEW VERSION CHECK GAME()let playerSelection = prePlayerSelection.toLowerCase(); //toString() converts the array^ from object to a string only then can it be used with .toLowerCase() , also switched from const to let because player choice is never "constant"
+//const computerSelection = computerPlay();
 let playerScore = 0;
 let compScore = 0;
 
@@ -46,26 +51,22 @@ let compScore = 0;
 
 function game(){ //***IMPORTANT:***REFER TO BOTTOM NOTES FOR IMPORTANT ANALYSIS ON HOW U GOT IT TO WORK PROPER!!!!***
   for(let i = 0; i < 5; i++){
-    let prePlayerSelection = prompt("Choose: Rock, Paper, Scissors?"); //
-    let playerSelection = prePlayerSelection.toString().toLowerCase();
     const computerSelection = computerPlay();
+    const playerSelection = prompt("Choose: Rock, Paper, or Scissors?").toLowerCase();
     console.log(singleRound(playerSelection , computerSelection));
     console.log("Your Score:" + playerScore);
     console.log("Comp Score:" + compScore);
   }
   
   if(playerScore > compScore) {
-    console.log(`Congrats! You win the game! Final Score: ${playerScore}`);
+    console.log(`CONGRATS! YOU WON THE GAME! FINAL SCORE: ${playerScore}`);
   } else {
-    console.log("Sorry :[ You have lost the game. Try Again!");
+    console.log("SORRY :[ YOU LOSE THE GAME. TRY AGAIN!");
   }
 }
 
 game(); //< this is needed to call the game func and displays all game's console.log in console!
 
-
-//***HAVE PLAYER SELECTION SHOW UP IN THE CONSOLE LOG*** */
-  
 
 
 // [DONE] c) use prompt() to get input from user
@@ -75,7 +76,11 @@ game(); //< this is needed to call the game func and displays all game's console
 
 //PROJECT NOTES:
 // NOTE(6/14/22): can input whatever casing in options but result comes out as lowercase to do func. Try to make it first letter capitalized
-// NOTE(6/14/22): prompt pops up 6x times not 5 and it doesnt count first choice, only starts to count after 2nd input. FIX THIS!
+/* [SOLVED!]NOTE(6/14/22): prompt pops up 6x times not 5 and it doesnt count first choice, only starts to count after 2nd input. FIX THIS! 
+    think problem is somewhere in game(). It is not in:
+      - console.log(singleRound(playerSelection,computerSelection); <---- [TESTED]
+    think it has something to do wit the upper section of the code, might be easier/need to : rework playerSelection and prePlayerSelection var/const!!!!!*/
+
 
 /*{{{FUNC game() ANALYSIS: 
 Before the same result kept displaying for all 5 rounds because: it was just calling that same "singleround()" 5x, 
@@ -85,4 +90,14 @@ HOW WE GOT IT TO WORK:
 It was crucial to redeclare the "preplayerSelection" prompt() to get the prompt to appear 5x so that the player can input 
 a different choice for every round if they want to! It was also crucial to redeclare the "playerSelection" var and 
 "computerSelection" const in the for() loop of the "game()" func, on top of the three console.log()s. The logic 
-behind this being that: redeclaring this var & const inside the loop calls a brand new different "singleRound()" func everytime for 5 different rounds. */
+behind this being that: redeclaring this var & const inside the loop calls a brand new different "singleRound()" func everytime for 5 different rounds. 
+
+6/14/22 NEW ERROR: REFER TO SECOND PROJECT NOTE ABOUT PROMPT AND SCORE MANAGEMENT!
+
+6/15/22 4:24pm: even by just leaving a var ex: let playerSelection = prompt(Choose: r,p or s?); it doesnt fix the issue of 6x prompt
+  
+  UPDATE!:4:37PM: WE GOT IT TO WORK!!!! How I got the prompt to show up 5x only and start counting from first choice:
+    - Taking the declared const playerSelection = prompt(R,P or S?).LowerCase() and const computerSelection = computerplay() out of the global scope
+    and only declaring it inside for() loop of the game() func scope!!! 
+      QUESTION: How does this all work properly w/o needing to declare the const earlier? IDK but it works! Might be because game() mends everything 
+        together after writing out all the proper var, func preceding it with everything else needed, declared within the game() func */
